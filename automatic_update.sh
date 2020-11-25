9d916e8afc96942fbeab56d56181aa1bdd107d4c
markuswinnen <markus@avalonslab.dev>
2020-11-25 19:58:43 +0100

    Update automatic_update.sh

--- automatic_update.sh
+++ automatic_update.sh
@@ -14,6 +14,7 @@ state="$(echo ${version_info} | jq --raw-output '.result')"
 log() {
   echo "[$(date --rfc-3339=seconds)] [Automatic Update] $*"
 }
+echo
 
 if [[ "${state}" != "success" ]]; then
   log "Cannot fetch version info from 'https://api.pchain.org/getLastVersion' > exiting"
